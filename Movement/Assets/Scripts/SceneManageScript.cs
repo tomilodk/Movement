99028d0e1c6b00dc1bf185c847d5de539bbe7aba
tomilodk <tomilobas@gmail.com>
2019-01-30 18:50:15 +0100

    Create Group

---
+++ Movement/Assets/Scripts/SceneManageScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManageScript : MonoBehaviour
+{
+    public void SwitchToMainScreen()
+    {
+        SceneManager.LoadScene("HomeScreen");
+    }
+}
